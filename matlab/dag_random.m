9dd4bcdc7bc4861f9134a3cc13c854736bc7ff05
jc38x <jcds.x38e.@gmail.com>
2016-02-26 14:01:13 -0800

    random

--- matlab/dag_random.m
+++ matlab/dag_random.m
@@ -5,16 +5,20 @@ t = tic();
 %[delay, range] = sample_valavan();
 %[delay, range] = random_dag(100, 5000, 60, 1, 10); %ok
 
-[delay, range] = random_dag(385, 555, 60, 1, 10); %ok
+%[delay, range] = random_dag(1, 495, 1, 1, 10);
+%[delay, range] = random_dag(385, 555, 60, 1, 10); %ok
 %[delay, range] = random_dag(100, 200, 10, 1, 10); %ok
-%[delay, range] = random_dag(20, 30, 10, 1, 10); %ok
+[delay, range] = random_dag(20, 30, 10, 1, 10); %ok
 %[delay, range] = random_dag(6, 6, 2, 1, 10); %ok
 toc(t)
 
 
+
+[labels] = node_labels(range);
+
 bg = build_graph(delay, labels, range);
 view(bg);
-%{
+
 
 t = tic();
 [labels] = node_labels(range);
@@ -45,5 +49,5 @@ newlabels = [labels(range.pi), labels(indices(I)+double(range.szpi)), labels(ran
 br = build_graph(result_delay, newlabels, result_range);
 
 view(br);
-
+%{
 %}
--- matlab/random_dag.m
+++ matlab/random_dag.m
@@ -8,14 +8,17 @@ function [out_delay, out_range] = random_dag(in_npi, in_nin, in_npo, in_mind, in
 sz = in_npi + in_nin + in_npo;
 out_delay = spalloc(sz, sz, (2 * in_nin) + in_npo);
 inofs = in_npi;
+inbse = in_npi+1;
 poofs = in_npi + in_nin;
 iein = zeros(1, in_nin);
 oein = zeros(1, in_nin);
 
+
+
 for spo = randperm(in_npo)
     sin = datasample(find(oein == min(oein)), 1);
+    out_delay(sin + inofs, spo + poofs) = random_delay();
     oein(sin) = oein(sin) + 1;
-    new_edge(sin + inofs, spo + poofs);
 end
 
 solstop = false;
@@ -23,26 +26,20 @@ solg = [];
 soliein = [];
 
 try_fill(out_delay, oein, iein);
-
 if (~solstop), error('Imposible'); end
 
 out_delay = solg;
 iein = soliein;
 
-takenpiin = zeros(1, in_npi);
-
 for spi = randperm(in_npi);
     fin = find(iein < 2);
     if (isempty(fin)), error('Imposible'); end
-    sin = datasample(fin, 1);
-    takenpiin(spi) = sin;
+    sin = datasample(fin, 1);    
+    out_delay(spi, sin + inofs) = random_delay();
     iein(sin) = iein(sin) + 1;
-    new_edge(spi, sin + inofs);
 end
 
-takeninin = true(in_nin, poofs);
-for i = 1:in_nin, takeninin(i, i + inofs) = false; end
-for i = 1:in_npi, takeninin(takenpiin(i), i) = false; end
+takeninin = full(out_delay(1:(poofs), (inbse):(poofs)).' <= 0);
 piin = 1:poofs;
 
 while (~all(iein >= 2))
@@ -57,14 +54,14 @@ while (~all(iein >= 2))
     adjtail = tail + inofs;
     takeninin(tail, head) = false;    
     if (~try_new_edge(out_delay, head, adjtail)), continue; end
-    new_edge(head, adjtail);
+    out_delay(head, adjtail) = random_delay();
     iein(tail) = iein(tail) + 1;
 end
 
 i1 = 1;
 in = in_npi;
-v1 = in + 1;
-vn = in + in_nin;
+v1 = inbse;
+vn = poofs;
 o1 = vn + 1;
 on = vn + in_npo;
 out_range = prepare_range(i1, in, v1, vn, o1, on);
@@ -73,9 +70,30 @@ function [out_delay] = random_delay()
     out_delay = randi([in_mind, in_maxd]);
 end
 
-function new_edge(in_o, in_i)
-    out_delay(in_o, in_i) = random_delay();
-end
+
+%function new_edge(in_o, in_i)
+%    out_delay(in_o, in_i) = random_delay();
+%end
+
+
+
+
+
+%takenpiin = zeros(1, in_npi);
+    %takenpiin(spi) = sin;
+
+%{
+takeninin = true(in_nin, poofs);
+for i = 1:in_nin, takeninin(i, i + inofs) = false; end
+for i = 1:in_npi, takeninin(takenpiin(i), i) = false; end
+    %}
+
+
+
+
+
+
+
 
 function [out_bool] = try_new_edge(in_g, in_o, in_i)
     if ((in_g(in_o, in_i) > 0) || (in_o == in_i))
