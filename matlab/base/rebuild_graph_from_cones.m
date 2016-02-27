4b5c6429f51d357cd22cf1012b924b8675ae3759
jc38x <jc38x@users.noreply.github.com>
2016-02-26 22:30:17 -0800

    rebuild patch

--- matlab/base/rebuild_graph_from_cones.m
+++ matlab/base/rebuild_graph_from_cones.m
@@ -7,42 +7,36 @@
 function [out_delay, out_tag, out_range] = rebuild_graph_from_cones(in_S, in_Cv, in_delay, in_range)
 nin = numel(in_S);
 nsz = in_range.szpi + nin + in_range.szpo;
-nzm = ((nsz - 1) * nsz) / 2;
 ofs = in_range.pihi;
 poofs = in_range.szin - nin;
-out_delay = spalloc(nsz, nsz, nzm);
+
+out_delay = spalloc(nsz, nsz, sum(in_delay(:) > 0));
 out_tag = sparse(1, [in_S - ofs], [[1:nin] + ofs]);
 
 for visible = in_S
     cvidx = visible - ofs;
     vcone = in_Cv{cvidx};
-    i2 = out_tag(cvidx);    
+    i2 = out_tag(cvidx);
     for e = vcone{3}
         e1 = e(1);
-        if (test_in(e1)), i1 = out_tag(e1 - ofs); else i1 = e1; end
-        make_edge(i1, i2, in_delay(e1, e(2)));
+        if (is_in(e1, in_range)), i1 = out_tag(e1 - ofs); else i1 = e1; end
+        delay = in_delay(e1, e(2));
+        if (delay > out_delay(i1, i2)), out_delay(i1, i2) = delay; end
     end
     for e = vcone{4}
         e2 = e(2);
-        if (test_in(e2)), continue; end
+        if (is_in(e2, in_range)), continue; end
         i1 = e2 - poofs;
-        make_edge(i2, i1, in_delay(e(1), e2));
+        delay = in_delay(e(1), e2);
+        if (delay > out_delay(i2, i1)), out_delay(i2, i1) = delay; end
     end
 end
 
 pilo = in_range.pilo;
 pihi = in_range.pihi;
 inlo = in_range.pihi + 1;
-inhi = in_range.pihi + numel(in_S);
+inhi = in_range.pihi + nin;
 polo = inhi + 1;
 pohi = inhi + in_range.szpo;
-out_range = prepare_range(pilo, pihi, inlo, inhi, polo, pohi); 
-
-function [out_bool] = test_in(in_node)
-    out_bool = is_in(in_node, in_range);
-end
-
-function make_edge(in_i1, in_i2, in_length)
-    out_delay(in_i1, in_i2) = in_length;
-end
+out_range = prepare_range(pilo, pihi, inlo, inhi, polo, pohi);
 end
--- matlab/dag_random.m
+++ matlab/dag_random.m
@@ -1,6 +1,9 @@
 
 K = 4;
 
+%RNG = rng();
+%rng(RNG);
+
 t = tic();
 %[delay, range] = sample_valavan();
 %[delay, range] = random_dag(100, 5000, 60, 1, 10); %ok
@@ -9,18 +12,10 @@ t = tic();
 %[delay, range] = random_dag(385, 555, 60, 1, 10); %ok
 %[delay, range] = random_dag(100, 200, 10, 1, 10); %ok
 %[delay, range] = random_dag(20, 30, 10, 1, 10); %ok
-[delay, range] = random_dag(1, 450, 1, 1, 10); %ok
+[delay, range] = random_dag(1, 10, 1, 1, 10); %ok
 %[delay, range] = random_dag(6, 6, 2, 1, 10); %ok
 toc(t)
 
-
-
-[labels] = node_labels(range);
-
-bg = build_graph(delay, labels, range);
-view(bg);
-
-%{
 t = tic();
 [labels] = node_labels(range);
 
@@ -31,11 +26,13 @@ redro = fliplr(order);
 depth = fill_depth(order, iedge, delay, range);
 height = fill_height(redro, oedge, delay, range);
 [af, noedge] = fill_af(order, iedge, oedge, range);
+toc(t)
 
-
-
+t = tic();
 [s, cv] = hara(order, iedge, oedge, noedge, delay, depth, height, af, range, K, false, 20);
+toc(t)
 
+t = tic();
 [result_delay, resulttag, result_range] = rebuild_graph_from_cones(s, cv, delay, range);
 toc(t)
 
@@ -46,10 +43,12 @@ indices = find(resulttag);
 [~, I] = sort(resulttag(indices));
 newlabels = [labels(range.pi), labels(indices(I)+double(range.szpi)), labels(range.po)];
 
+bg = build_graph(delay, labels, range);
+view(bg);
 
 br = build_graph(result_delay, newlabels, result_range);
 
 view(br);
-%}
+
 %{
 %}
