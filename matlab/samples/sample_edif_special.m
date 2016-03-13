21978c9243ebb1af65ddb9b1f1e40f897f95614e
jc38x <jc38x@users.noreply.github.com>
2016-03-13 13:45:48 -0700

    more samples

---
+++ matlab/samples/sample_edif_special.m
@@ -0,0 +1,12 @@
+%**************************************************************************
+% X38-02FO16
+% jcds (jcds.x38e@gmail.com)
+% 2016
+%**************************************************************************
+% References
+% https://fedorahosted.org/fedora-electronic-lab/attachment/ticket/77/example.edif
+%**************************************************************************
+
+function [out_delay, out_labels, out_range] = sample_edif_special(in_flatten)
+[out_delay, out_labels, out_range] = edif2mat([mfilename('fullpath') '.edif'], in_flatten);
+end
