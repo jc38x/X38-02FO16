45d320107813a02a0f7c6e5342e4352bc8699c1a
jc38x <jc38x@users.noreply.github.com>
2016-03-29 17:09:21 -0700

    header

--- matlab/lib/is_odd.m
+++ matlab/lib/is_odd.m
@@ -3,6 +3,9 @@
 % jcds (jcds.x38e@gmail.com)
 % 2016
 %**************************************************************************
+% References
+% http://www.mathworks.com/matlabcentral/answers/178217-is-there-built-in-function-such-as-iseven-isint-and-factorial-in-matlab
+%**************************************************************************
 
 function [out_isit] = is_odd(in_n)
 out_isit = mod(in_n, 2) == 1;
--- matlab/lib/strsplitntrim.m
+++ matlab/lib/strsplitntrim.m
@@ -3,6 +3,9 @@
 % jcds (jcds.x38e@gmail.com)
 % 2016
 %**************************************************************************
+% References
+% http://stackoverflow.com/questions/5286420/how-to-vectorize-this-string-split-function-in-matlab
+%**************************************************************************
 
 function [out_str] = strsplitntrim(in_str, in_delimiters)
 out_str = textscan(in_str, '%s', 'delimiter', in_delimiters, 'MultipleDelimsAsOne', 1);
