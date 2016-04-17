fdcb921c80f57306f8b97c7b45a80bd471447e63
jc38x <jc38x@users.noreply.github.com>
2016-04-17 14:00:24 -0700

    leko_leku

--- matlab/interface/load_leko_leku.m
+++ matlab/interface/load_leko_leku.m
@@ -7,7 +7,7 @@
 function [out_delay, out_labels, out_range, out_equations] = load_leko_leku(in_name)
 [dirs, sl] = split_module_path(mfilename('fullpath'));
 base = [dirs{1:(end-2)}];
-path = [base 'benchmarks' sl 'LEKO_LEKU' sl in_name];
+path = [base 'benchmarks' sl 'LEKO_LEKU' sl in_name '.blif'];
 if (exist(path, 'file') ~= 2), error(['File ' path ' not found.']); end
 tmp = [base 'workspace' sl 'tmp_lek__blif2aig_abc.aig'];
 invoke_abc({['read_blif ' path ';']; 'strash'; ['write_aiger -s ' tmp ';']; 'quit'}, false);
