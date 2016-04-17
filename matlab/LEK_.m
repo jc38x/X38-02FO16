a562f64e9afe2827ce3cfcdd210632195f2be999
jc38x <jc38x@users.noreply.github.com>
2016-04-17 14:00:16 -0700

    lgsynth93

--- matlab/LEK_.m
+++ matlab/LEK_.m
@@ -3,15 +3,16 @@ K = 4;
 DF = false;
 mode = 2;
 alpha = 1.5; %1.5-2.5
-maxi = 1; %20
+maxi = 20; %20
 epsrand = [0.000, 0.000]; %small
 
 
-[delay, labels, range, equations] = load_leko_leku('leko-g5\g25.blif');
+%[delay, labels, range, equations] = load_leko_leku('leko-g5\g25');
+[delay, labels, range, equations] = load_lgsynth93('blif\alu4');
 
 filename = 'C:/Users/jcds/Documents/GitHub/X38-02FO16/workspace/lekog25.aig';
 mat2aiger(filename, delay, labels, range, equations);
-
+%{
 script = [
     {['read_aiger ' filename]};
     {'cleanup'};
@@ -20,6 +21,15 @@ script = [
     {'cec'}
     {'quit'};
     ];
+%}
+script = {
+    'resyn2';
+    'resyn2rs';
+    'resyn2rs';
+    'resyn2rs';
+    'resyn2rs';
+    'resyn2rs';
+    };
 
 invoke_abc(script, false);
 
@@ -32,7 +42,9 @@ depth = fill_depth(order, iedge, delay, range);
 height = fill_height(redro, oedge, delay, range);
 [af, noedge] = fill_af(order, iedge, oedge, range);
 
+t = tic();
 [s, cv] = hara(order, iedge, oedge, noedge, delay, depth, height, af, range, K, DF, mode, maxi, alpha, epsrand(1), epsrand(2));
+toc(t);
 
 [resultdf, resultlf, resultrf, resultef] = rebuild_graph_from_cones(s, cv, delay, range, labels, equations);
 [lutsf] = cones2luts(resultdf, resultlf, resultrf, resultef);
---
+++ matlab/interface/load_lgsynth93.m
@@ -0,0 +1,15 @@
+%**************************************************************************
+% X38-02FO16
+% jcds (jcds.x38e@gmail.com)
+% 2016
+%**************************************************************************
+
+function [out_delay, out_labels, out_range, out_equations] = load_lgsynth93(in_name)
+[dirs, sl] = split_module_path(mfilename('fullpath'));
+base = [dirs{1:(end-2)}];
+path = [base 'benchmarks' sl 'LGSynth93' sl 'testcases' sl in_name '.blif'];
+if (exist(path, 'file') ~= 2), error(['File ' path ' not found.']); end
+tmp = [base 'workspace' sl 'tmp_lgsynth93_blif2aig_abc.aig'];
+invoke_abc({['read_blif ' path ';']; 'strash'; ['write_aiger -s ' tmp ';']; 'quit'}, false);
+[out_delay, out_labels, out_range, out_equations] = aiger2mat(tmp);
+end
